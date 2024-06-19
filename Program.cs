9f3f2d7e9f83dd24392b2dc9b44586379c267ecb
Luccas Carneiro <luccas.carneiro@smartconsulting.com.br>
2024-06-19 02:56:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Entity entity = new Entity();
+            entity.LogicalName = "account";
+            entity.Attributes["name"] = "Test Account";
+            entity.Attributes["address1_city"] = "Redmond";
+            entity.Attributes["address1_stateorprovince"] = "WA";
+            entity.Attributes["address1_country"] = "US";
+            entity.Id = Guid.NewGuid();
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Entity: " + entity.LogicalName);
+            Console.WriteLine("Name: " + entity.Attributes["name"]);
+            Console.WriteLine("City: " + entity.Attributes["address1_city"]);
+            Console.WriteLine("State: " + entity.Attributes["address1_stateorprovince"]);
+            Console.WriteLine("Country: " + entity.Attributes["address1_country"]);
+            Console.WriteLine("Id: " + entity.Id);
+
+        }
+    }
+}
+
+
